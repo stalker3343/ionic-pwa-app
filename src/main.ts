f715efb6c0ae55e61ebe8bb44a94ee6767bc64f2
Artem Byastic <stalker3343@gmail.com>
2021-03-12 10:56:05 +0300

    init pwa by cli-plugin-pwa

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+// Added by the CLI
+import './registerServiceWorker'
 
 import { IonicVue } from '@ionic/vue';
 
